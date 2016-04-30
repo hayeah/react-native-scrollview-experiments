e3ee32b32eeac80534641f4de4dd6be19ae820ae
Howard Yeh <howard@metacircus.com>
2016-04-30 11:31:24 -0700

    fixed scrollview compositing

---
+++ compositing-scrollview-fixed.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+import {
+  AppRegistry,
+
+  ScrollView,
+  StyleSheet,
+  View,
+} from "react-native";
+
+export class App extends React.Component {
+  render() {
+    return (
+      <ScrollView style={jss.scrollView}
+        contentContainerStyle={jss.contentContainerStyle}>
+        <View style={jss.box}/>
+        <View style={jss.box}/>
+        <View style={jss.box}/>
+        <View style={jss.box}/>
+      </ScrollView>
+    );
+  }
+}
+
+const jss = StyleSheet.create({
+  scrollView: {
+    backgroundColor: 'lightgray'
+  },
+
+  contentContainerStyle: {
+    alignItems: 'center',
+    paddingTop: 50,
+    borderWidth: 1,
+    borderColor: 'red',
+    backgroundColor: 'lightgray',
+  },
+
+  box: {
+    width: 300,
+    height: 300,
+    backgroundColor: 'gray',
+    marginBottom: 10,
+  }
+});
+
+AppRegistry.registerComponent('SampleApp', () => App);
