be58ef42342b909e3df291dc835312ec83befdb0
Howard Yeh <howard@metacircus.com>
2016-05-02 16:34:34 -0700

    add pager demo

---
+++ pager-android.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Dimensions,
+
+  ViewPagerAndroid,
+  View,
+  Text,
+} from "react-native";
+
+
+export class App extends React.Component {
+  render() {
+    const windowSize = Dimensions.get("window");
+
+    const fullScreenSize = {
+      width: windowSize.width,
+      height: windowSize.height,
+    };
+
+    console.log("App#render", fullScreenSize);
+    return (
+      <ViewPagerAndroid
+        style={fullScreenSize}
+        >
+        <View style={[
+          jss.page,
+          { backgroundColor: 'rgba(255,0,0,0.3)' },
+        ]}>
+          <Text>Page 1</Text>
+        </View>
+
+        <View style={[
+          jss.page,
+          { backgroundColor: 'rgba(0,255,0,0.3)' },
+        ]}>
+          <Text>Page 2</Text>
+        </View>
+
+        <View style={[
+          jss.page,
+          { backgroundColor: 'rgba(0,0,255,0.3)' },
+        ]}>
+          <Text>Page 3</Text>
+        </View>
+
+      </ViewPagerAndroid>
+    );
+  }
+}
+
+const jss = StyleSheet.create({
+  page: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+AppRegistry.registerComponent('SampleApp', () => App);
---
+++ pager-ios.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Dimensions,
+
+  ScrollView,
+  View,
+  Text,
+} from "react-native";
+
+const windowSize = Dimensions.get("window");
+
+export class App extends React.Component {
+  render() {
+    const windowSize = Dimensions.get("window");
+
+    const fullScreenSize = {
+      width: windowSize.width,
+      height: windowSize.height,
+    };
+
+    return (
+      <ScrollView
+        horizontal={true}
+        pagingEnabled={true}
+        bounces={true}
+        >
+
+        <View style={[
+          jss.page,
+          fullScreenSize,
+          { backgroundColor: 'rgba(255,0,0,0.3)' },
+        ]}>
+          <Text>Page 1</Text>
+        </View>
+
+        <View style={[
+          jss.page,
+          fullScreenSize,
+          { backgroundColor: 'rgba(0,255,0,0.3)' },
+        ]}>
+          <Text>Page 2</Text>
+        </View>
+
+        <View style={[
+          jss.page,
+          fullScreenSize,
+          { backgroundColor: 'rgba(0,0,255,0.3)' },
+        ]}>
+          <Text>Page 3</Text>
+        </View>
+    </ScrollView>
+    );
+  }
+}
+
+const jss = StyleSheet.create({
+  page: {
+    backgroundColor: 'rgba(255,0,0,0.3)',
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+AppRegistry.registerComponent('SampleApp', () => App);
